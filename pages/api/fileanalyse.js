092cadb6524e702bb79d3fa3a00f0244ed60c385
robdll <dilillo.roberto@gmail.com>
2022-06-29 00:27:13 +0100

    adds endpoint

--- pages/api/fileanalyse.js
+++ pages/api/fileanalyse.js
@@ -1,9 +1,19 @@
-export default function handler(req, res) {
-  res.setHeader("Content-Type", "application/json");
-  const data = {
-    ipaddress: req.headers["x-forwarded-for"],
-    language: req.headers["accept-language"],
-    software: req.headers["user-agent"],
-  };
-  res.status(200).json(data);
+import multer from "multer";
+
+// disable next.js' default body parser
+export const config = {
+  api: { bodyParser: false },
+};
+
+export default async function handler(req, res) {
+  await new Promise((resolve) => {
+    const mw = multer().any();
+    mw(req, res, resolve);
+  });
+
+  res.status(200).json({
+    name: req.files[0].originalname,
+    type: req.files[0].mimetype,
+    size: req.files[0].size,
+  });
 }
